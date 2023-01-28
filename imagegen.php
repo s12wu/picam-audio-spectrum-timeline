83e0f237524f4ce8793548d3fa117e9d31739d1f
s12wu <90598549+s12wu@users.noreply.github.com>
2023-01-28 12:54:33 +0100

    Improve image loading for better performance
    Don't load the images to the left of the visible area
    Use imagecopyresized instead of imagecopyresampled. (No resampling = Lower quality, but faster)

--- imagegen.php
+++ imagegen.php
@@ -72,20 +72,20 @@ while (($line = fgets($file)) !== false) {
 
         $addmore = 1;
         while ($addmore == 1) {
-            $spectrum_img = LoadJpeg("spectrum/$videoid/" . $spectrum_img_list[$spectrum_img_list_idx]);
-
             $x = $start_x + ($spectrum_img_list_idx-2) * $spectrum_img_width_at_current_range;
 
-            imagecopyresampled($image, $spectrum_img, $x, 0, 0, 0, $spectrum_img_width_at_current_range, $height, imagesx($spectrum_img), imagesy($spectrum_img));
+            if (x + $spectrum_img_width_at_current_range > 0) { //would this image even be visible?
+                $spectrum_img = LoadJpeg("spectrum/$videoid/" . $spectrum_img_list[$spectrum_img_list_idx]);
+                imagecopyresized($image, $spectrum_img, $x, 0, 0, 0, $spectrum_img_width_at_current_range, $height, imagesx($spectrum_img), imagesy($spectrum_img));
 
-            $spectrum_img_list_idx++;
+                $spectrum_img_list_idx++;
 
-            if ($x > $width || $spectrum_img_list_idx >= count($spectrum_img_list)) {
-                $addmore = 0;
+                if ($x > $width || $spectrum_img_list_idx >= count($spectrum_img_list)) {
+                    $addmore = 0;
+                }
             }
 
         }
-
     }
 
 }
